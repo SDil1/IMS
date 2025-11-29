6abc4355ea0c6076a0f0090e7f4d9f876d6261fd
SDIL1 <senudadi1@gmail.com>
2025-11-29 15:09:40 +0530

    customer db added & connected

--- InventoryMnagement/ManageCustomers.Designer.cs
+++ InventoryMnagement/ManageCustomers.Designer.cs
@@ -29,11 +29,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.closeButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.FullName = new System.Windows.Forms.Label();
-            this.password = new System.Windows.Forms.Label();
             this.phone_num = new System.Windows.Forms.Label();
             this.username = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -42,11 +41,10 @@
             this.delete = new System.Windows.Forms.Button();
             this.edit = new System.Windows.Forms.Button();
             this.Users = new System.Windows.Forms.DataGridView();
-            this.Ufullname = new System.Windows.Forms.TextBox();
-            this.Uname = new System.Windows.Forms.TextBox();
-            this.Upassword = new System.Windows.Forms.TextBox();
-            this.Uphone = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.custname = new System.Windows.Forms.TextBox();
+            this.custphone = new System.Windows.Forms.TextBox();
+            this.custId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Users)).BeginInit();
@@ -64,6 +62,16 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1452, 100);
             this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1412, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // closeButton
             // 
@@ -94,31 +102,11 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "INVENTORY MANAGEMENT SYSTEM";
             // 
-            // FullName
-            // 
-            this.FullName.AutoSize = true;
-            this.FullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FullName.Location = new System.Drawing.Point(12, 121);
-            this.FullName.Name = "FullName";
-            this.FullName.Size = new System.Drawing.Size(118, 25);
-            this.FullName.TabIndex = 10;
-            this.FullName.Text = "Full Name";
-            // 
-            // password
-            // 
-            this.password.AutoSize = true;
-            this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.password.Location = new System.Drawing.Point(15, 213);
-            this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(112, 25);
-            this.password.TabIndex = 9;
-            this.password.Text = "password";
-            // 
             // phone_num
             // 
             this.phone_num.AutoSize = true;
             this.phone_num.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.phone_num.Location = new System.Drawing.Point(15, 262);
+            this.phone_num.Location = new System.Drawing.Point(15, 208);
             this.phone_num.Name = "phone_num";
             this.phone_num.Size = new System.Drawing.Size(77, 25);
             this.phone_num.TabIndex = 8;
@@ -161,6 +149,7 @@
             this.add.TabIndex = 20;
             this.add.Text = "ADD";
             this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
             // 
             // delete
             // 
@@ -191,60 +180,51 @@
             this.Users.Size = new System.Drawing.Size(1104, 515);
             this.Users.TabIndex = 21;
             // 
-            // Ufullname
+            // custname
             // 
-            this.Ufullname.Location = new System.Drawing.Point(140, 126);
-            this.Ufullname.Name = "Ufullname";
-            this.Ufullname.Size = new System.Drawing.Size(164, 20);
-            this.Ufullname.TabIndex = 22;
+            this.custname.Location = new System.Drawing.Point(140, 166);
+            this.custname.Name = "custname";
+            this.custname.Size = new System.Drawing.Size(164, 20);
+            this.custname.TabIndex = 23;
             // 
-            // Uname
+            // custphone
             // 
-            this.Uname.Location = new System.Drawing.Point(140, 166);
-            this.Uname.Name = "Uname";
-            this.Uname.Size = new System.Drawing.Size(164, 20);
-            this.Uname.TabIndex = 23;
+            this.custphone.Location = new System.Drawing.Point(140, 208);
+            this.custphone.Name = "custphone";
+            this.custphone.Size = new System.Drawing.Size(164, 20);
+            this.custphone.TabIndex = 25;
             // 
-            // Upassword
+            // custId
             // 
-            this.Upassword.Location = new System.Drawing.Point(140, 219);
-            this.Upassword.Name = "Upassword";
-            this.Upassword.Size = new System.Drawing.Size(164, 20);
-            this.Upassword.TabIndex = 24;
+            this.custId.Location = new System.Drawing.Point(140, 124);
+            this.custId.Name = "custId";
+            this.custId.Size = new System.Drawing.Size(164, 20);
+            this.custId.TabIndex = 26;
             // 
-            // Uphone
+            // label3
             // 
-            this.Uphone.Location = new System.Drawing.Point(140, 268);
-            this.Uphone.Name = "Uphone";
-            this.Uphone.Size = new System.Drawing.Size(164, 20);
-            this.Uphone.TabIndex = 25;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(1412, 9);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(28, 23);
-            this.button1.TabIndex = 18;
-            this.button1.Text = "X";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 25);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "ID";
             // 
             // ManageCustomers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1452, 672);
-            this.Controls.Add(this.Uphone);
-            this.Controls.Add(this.Upassword);
-            this.Controls.Add(this.Uname);
-            this.Controls.Add(this.Ufullname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.custId);
+            this.Controls.Add(this.custphone);
+            this.Controls.Add(this.custname);
             this.Controls.Add(this.Users);
             this.Controls.Add(this.add);
             this.Controls.Add(this.delete);
             this.Controls.Add(this.edit);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.FullName);
-            this.Controls.Add(this.password);
             this.Controls.Add(this.phone_num);
             this.Controls.Add(this.username);
             this.Controls.Add(this.panel1);
@@ -266,8 +246,6 @@
         private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label FullName;
-        private System.Windows.Forms.Label password;
         private System.Windows.Forms.Label phone_num;
         private System.Windows.Forms.Label username;
         private System.Windows.Forms.Panel panel2;
@@ -276,10 +254,10 @@
         private System.Windows.Forms.Button delete;
         private System.Windows.Forms.Button edit;
         private System.Windows.Forms.DataGridView Users;
-        private System.Windows.Forms.TextBox Ufullname;
-        private System.Windows.Forms.TextBox Uname;
-        private System.Windows.Forms.TextBox Upassword;
-        private System.Windows.Forms.TextBox Uphone;
+        private System.Windows.Forms.TextBox custname;
+        private System.Windows.Forms.TextBox custphone;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox custId;
+        private System.Windows.Forms.Label label3;
     }
 }
--- InventoryMnagement/ManageCustomers.cs
+++ InventoryMnagement/ManageCustomers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,10 +17,58 @@ namespace InventoryMnagement
         {
             InitializeComponent();
         }
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\senud\Desktop\practise division\InventoryMnagement\InventoryMnagement\inventorydb.mdf"";Integrated Security=True;");
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        void populate()
+        {
+            try
+            {
+                con.Open();
+
+                string query = "select * from CustomerTbl";
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                var ds = new DataSet();
+                da.Fill(ds);
+                Users.DataSource = ds.Tables[0];
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(
+                "INSERT INTO CustomerTbl (custId,custName, custPhone) VALUES ('"
+                + custId.Text + "', '"
+                + custname.Text + "', '"
+                + custphone.Text + "')",
+                con);
+
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Customer Added Successfully");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            populate();
         }
     }
 }
